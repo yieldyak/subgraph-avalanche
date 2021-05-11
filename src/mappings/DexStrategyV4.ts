dd4fe56b56889055671cbf252415e46dee88dae9
snow-farmer <team@snowballs.finance>
2021-05-11 09:45:00 +0200

    Add strategies

--- src/mappings/DexStrategyV4.ts
+++ src/mappings/DexStrategyV4.ts
@@ -1,9 +1,52 @@
-import { Address, BigInt, BigDecimal } from '@graphprotocol/graph-ts'
+import { Address, BigInt, BigDecimal, ethereum } from '@graphprotocol/graph-ts'
 import { 
   DexStrategyV4,
-  Reinvest as ReinvestEvent
+  Deposit as DepositEvent,
+  Reinvest as ReinvestEvent,
+  Withdraw as WithdrawEvent,
 } from '../../generated/DexStrategyV4/DexStrategyV4'
-import { User, Farm, Token, Reinvest } from '../../generated/schema'
+import { User, Farm, Token, Reinvest, Deposit, Withdraw, DepositStatus } from '../../generated/schema'
+
+export function handleDeposit(event: DepositEvent): void {
+  let depositStatus = createOrLoadDepositStatus(event);
+  depositStatus.activeDeposit = depositStatus.activeDeposit.plus(event.params.amount);
+  depositStatus.depositCount = depositStatus.depositCount.plus(BigInt.fromI32(1));
+  depositStatus.totalDeposits = depositStatus.totalDeposits.plus(event.params.amount);
+  depositStatus.save()
+
+  let id = event.transaction.hash.toHexString() + "-" + event.transactionLogIndex.toString();
+  let deposit = new Deposit(id);
+  deposit.by = createOrLoadUser(event).id;
+  deposit.farm = createOrLoadFarm(event).id;
+  deposit.amount = event.params.amount;
+  deposit.blockTimestamp = event.block.timestamp;
+  deposit.blockNumber = event.block.number;
+  deposit.transactionHash = event.transaction.hash;
+  deposit.save();
+}
+
+export function handleWithdraw(event: WithdrawEvent): void {
+  let depositStatus = createOrLoadDepositStatus(event);
+  if (depositStatus.activeDeposit.lt(event.params.amount)) {
+    depositStatus.activeDeposit = BigInt.fromI32(0);
+  }
+  else {
+    depositStatus.activeDeposit = depositStatus.activeDeposit.minus(event.params.amount);
+  }
+  depositStatus.withdrawCount = depositStatus.withdrawCount.plus(BigInt.fromI32(1));
+  depositStatus.totalWithdraws = depositStatus.totalWithdraws.plus(event.params.amount);
+  depositStatus.save()
+
+  let id = event.transaction.hash.toHexString() + "-" + event.transactionLogIndex.toString();
+  let withdraw = new Withdraw(id);
+  withdraw.by = createOrLoadUser(event).id;
+  withdraw.farm = createOrLoadFarm(event).id;
+  withdraw.amount = event.params.amount;
+  withdraw.blockTimestamp = event.block.timestamp;
+  withdraw.blockNumber = event.block.number;
+  withdraw.transactionHash = event.transaction.hash;
+  withdraw.save();
+}
 
 export function handleReinvest(event: ReinvestEvent): void {
   let id = event.transaction.hash.toHexString() + "-" + event.transactionLogIndex.toString();
@@ -29,7 +72,7 @@ export function handleReinvest(event: ReinvestEvent): void {
   reinvest.save();
 }
 
-function createOrLoadFarm(event: ReinvestEvent): Farm {
+function createOrLoadFarm(event: ethereum.Event): Farm {
   let id = event.address.toHexString();
   let farm = Farm.load(id);
   if (farm == null) {
@@ -53,7 +96,7 @@ function createOrLoadToken(id: Address): Token {
   return token!;
 }
 
-function createOrLoadUser(event: ReinvestEvent): User {
+function createOrLoadUser(event: ethereum.Event): User {
   let id = event.transaction.from.toHexString();
   let user = User.load(id);
   if (user == null) {
@@ -62,4 +105,21 @@ function createOrLoadUser(event: ReinvestEvent): User {
   }
   user.save();
   return user!;
+}
+
+function createOrLoadDepositStatus(event: ethereum.Event): DepositStatus {
+  let id = event.transaction.from.toHexString() + "-" + event.address.toHexString();
+  let depositStatus = DepositStatus.load(id);
+  if (depositStatus == null) {
+    depositStatus = new DepositStatus(id);
+    depositStatus.farm = createOrLoadFarm(event).id;
+    depositStatus.user = createOrLoadUser(event).id;
+    depositStatus.activeDeposit = BigInt.fromI32(0);
+    depositStatus.totalDeposits = BigInt.fromI32(0);
+    depositStatus.totalWithdraws = BigInt.fromI32(0);
+    depositStatus.depositCount = BigInt.fromI32(0);
+    depositStatus.withdrawCount = BigInt.fromI32(0);
+  }
+  depositStatus.save();
+  return depositStatus!;
 }
